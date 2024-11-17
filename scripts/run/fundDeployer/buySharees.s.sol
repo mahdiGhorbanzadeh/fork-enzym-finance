9f762720a3aad0ccde3499dd7b7e7c36e206a2ae
smuggler <smuggachak.1999@gmail.com>
2024-11-17 20:26:42 +0330

    value Interpreter

---
+++ scripts/run/fundDeployer/buySharees.s.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity 0.8.19; 
+ 
+import "forge-std/Script.sol"; 
+import "forge-std/console.sol"; 
+ 
+ 
+interface IComptroller { 
+    
+    function buyShares(uint256 _investmentAmount, uint256 _minSharesQuantity) 
+        external 
+        returns (uint256 sharesReceived_);  
+ 
+} 
+ 
+contract BuyShares is Script { 
+ 
+    address public constant comptroller = 0x9CA4577C715F84567cA8b33cfeF96C41A74E10dE; 
+ 
+ 
+    function run() external { 
+         
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY"); 
+         
+        vm.startBroadcast(deployerPrivateKey); 
+ 
+        IComptroller comptrollerI = IComptroller(comptroller); 
+    
+        comptrollerI.buyShares(100,10); 
+ 
+        vm.stopBroadcast(); 
+ 
+    } 
+}
